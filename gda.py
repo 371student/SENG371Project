318f05214f6219d71577df68fe4eb0e8c95088bd
sarahannnicholson <sarah.ann.nicholson@gmail.com>
2015-02-14 11:35:11 -0800

    Update gda.py
    added a return value to parse_log() so that we can test the other commands
    changed print statement in main() to print out int and str in 1 line
    called git checkout in travel_to()
    made a list of tuples (output of sfood command) and stored it into a variable which we can parse in calculate_coupling_factor()

--- gda.py
+++ gda.py
@@ -1,64 +1,79 @@
 #!/usr/bin/python
-
 """
 Welcome to GDA!
 Usage instructions:
-  1. Navigate to the root directory of the git project you wish to analyze
-  2. git log --date-order > temp.txt
-  3. cat temp.txt | python /path/to/gda.py
+1. Navigate to the root directory of the git project you wish to analyze
+2. git log --date-order > temp.txt
+3. cat temp.txt | python /path/to/gda.py
 """
-
 import sys
 import subprocess
+from subprocess import call
+import os
 
 
 def parse_log():
-  """
-  Parse each line of sys.stdin and convert the logs into a nice format
-  For every yymm (2015-02 etc.) we want all of the commit ids
-  This should be stored as a dictionary with yymm as the key
-  and a list of commit ids as the value.
-  {'2015-02': [648e516b8d5694c01a225fe27429f0bf7776fb43, 648e516b8d5694c01a225fe27429f0bf7776fb43], '2015-01': [648e516b8d5694c01a225fe27429f0bf7776fb43]}
-  NOTE: If a yymm has no commits, it should not appear as a key
-  """
-  for line in sys.stdin:
-    pass
-  return {}
+	"""
+	Parse each line of sys.stdin and convert the logs into a nice format
+	For every yymm (2015-02 etc.) we want all of the commit ids
+	This should be stored as a dictionary with yymm as the key
+	and a list of commit ids as the value.
+	{'2015-02': [648e516b8d5694c01a225fe27429f0bf7776fb43, 648e516b8d5694c01a225fe27429f0bf7776fb43], '2015-01': [648e516b8d5694c01a225fe27429f0bf7776fb43]}
+	NOTE: If a yymm has no commits, it should not appear as a key
+	"""
+	#for line in sys.stdin:
+	#	pass
+	return {'2015-13': ['03d964a924e0ef183c3cd78a61c043b1f74f5570']}
+
 
 def travel_to(commit):
-  """
-  Run git checkout <commit> to reset the project to the state it was in after
-  the given commit
-  """
-  pass
+	"""
+	Run git checkout <commit> to reset the project to the state it was in after
+	the given commit
+	"""
+	call(["git", "checkout" , commit])
+	#pass
+
 
 def calculate_coupling_factor():
-  """
-  Run sfood and analyze its output to calculate a 'coupling factor'.
-  This should be the number of 'edges' divided by the number of 'vertices'.
-  """
-  return 42
+	"""
+	Run sfood and analyze its output to calculate a 'coupling factor'.
+	This should be the number of 'edges' divided by the number of 'vertices'.
+	"""
+	""" list_of_tuples will hold a list of each tuple outputted by running sfood
+	eg) 
+	(('/home/sarah/pyramid/pyramid/tests/test_scaffolds/fixture_scaffold/+package+', 'resources.py'), (None, None))
+	('/home/sarah/pyramid', 'pyramid/view.py'))
+	etc... """
+	list_of_tuples = []
+
+	os.system("sfood > sfood_output.txt")
+	sfood_output = open("sfood_output.txt")
+	for line in sfood_output:
+		list_of_tuples.append(line)
+
+	return 42
+
 
 def analyze(commits):
-  """
-  Analyze the 'coupling factor' for each of the commits given.
-  Return the average of all calculated 'coupling factors' over these commits
-  """
-  coupling_factors = []
-  for commit in commits:
-    travel_to(commit)
-    coupling_factors.append(calculate_coupling_factor())
+	"""
+	Analyze the 'coupling factor' for each of the commits given.
+	Return the average of all calculated 'coupling factors' over these commits
+	"""
+	coupling_factors = []
+	for commit in commits:
+		travel_to(commit)
+		coupling_factors.append(calculate_coupling_factor())
 
-  average = sum(coupling_factors)/float(len(coupling_factors))
-
-  return 42
+	average = sum(coupling_factors)/float(len(coupling_factors))
+	return 42
 
 
 def main():
-    history = parse_log()
-    for yymm in history:
-      coupling_factor = analyze(history[yymm])
-      print yymm + ',' + coupling_factor + ';'
+	history = parse_log()
+	for yymm in history:
+		coupling_factor = analyze(history[yymm])
+		print yymm + "," , coupling_factor 
 
 if __name__ == '__main__':
-    main()
+	main()
