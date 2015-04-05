aca9b2b36423ff2b1eb65756c4fcfa86e1d4fd72
chrisjcook <chris@chriscook.ca>
2015-04-04 21:16:00 -0700

    scaling back api

--- public/app/services/repoService.js
+++ public/app/services/repoService.js
@@ -5,24 +5,14 @@ angular.module('repoService', [])
 		// create a new object
 		var repoFactory = {};
 
-		// get a single repo's data
-		repoFactory.get = function(repoUrl) {
-			return $http.get('/api/repos/' + repoUrl);
-		};
-
-		// get a every repo's data
+		// get every repo's data
 		repoFactory.all = function() {
-			return $http.get('/api/repos/');
+			return $http.get('/api/repos');
 		};
 
 		// add a new repo's data
 		repoFactory.add = function(repoUrl) {
-			return $http.post('/api/repos/', repoUrl);
-		};
-
-		// update a repo's data
-		repoFactory.update = function(repoUrl) {
-			return $http.put('/api/repos/', repoUrl);
+			return $http.post('/api/add', repoUrl);
 		};
 
 		// return our entire repoFactor object
