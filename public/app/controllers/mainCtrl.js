0c4d848fcb74fbf8a716389a8b66bfc50d04d234
chrisjcook <chris@chriscook.ca>
2015-04-21 01:45:24 -0700

    connecting submit button

--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -6,23 +6,6 @@ angular.module('mainCtrl', ['repoService'])
 	// bind "this" to vm (view-model)
 	var vm = this;
 
-	// define a list of GitHub repositories
-	// we will eventually just pull this list from the DB
-	
-	// vm.repositories = [
-	// 	{ name: 'Django', url: 'https://github.com/django/django', status: 'Complete' },
-	// 	{ name: 'Pyramid', url: 'https://github.com/Pylons/pyramid', status: 'Complete' },
-	// 	{ name: 'Flask', url: 'https://github.com/mitsuhiko/flask', status: 'Complete' },
-	// 	{ name: 'Test', url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', status: 'In Progress' },
-	// 	{ name: 'Test', url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', status: 'In Progress' },
-	// 	{ name: 'Test', url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', status: 'In Progress' },
-	// 	{ name: 'Test', url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', status: 'In Progress' },
-	// 	{ name: 'Test', url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', status: 'In Progress' },
-	// 	{ name: 'Test', url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', status: 'In Progress' },
-	// 	{ name: 'Test', url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', status: 'In Progress' },
-	// 	{ name: 'Test', url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', status: 'In Progress' }
-	// ];
-
 	// Get all the repositories
 	Repo.all()
 		// Make promise
@@ -31,6 +14,14 @@ angular.module('mainCtrl', ['repoService'])
 		});
 
 	vm.submitRepoUrl = function() {
-		$location.path('/data');
+		vm.processing = true;
+
+		Repo.add(vm.newrepo.repoUrl)
+			// Make promise
+			.success(function(data) {
+				vm.processing = false;
+
+				// $location.path('/data');
+			});
 	};
 });
