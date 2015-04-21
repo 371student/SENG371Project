2fdfa4afb9c1ad206fd89f42666895a0c192fc5f
chrisjcook <chris@chriscook.ca>
2015-04-21 02:32:09 -0700

    getting repoService to play nice with main app

--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -1,27 +1,25 @@
 angular.module('mainCtrl', ['repoService'])
 
-// create the controllers
-// this will be the controller for the ENTIRE site
-.controller('mainController', function($location, Repo) {
-	// bind "this" to vm (view-model)
-	var vm = this;
+	.controller('mainController', function($location, Repo) {
+		// bind "this" to vm (view-model)
+		var vm = this;
 
-	// Get all the repositories
-	Repo.all()
-		// Make promise
-		.success(function(data) {
-			vm.repositories = data;
-		});
-
-	vm.submitRepoUrl = function() {
-		vm.processing = true;
-		
-		Repo.add(vm.repoUrl)
+		// Get all the repositories
+		Repo.all()
+			// Make promise
 			.success(function(data) {
-				vm.processing = false;
-
-				vm.repoUrl = null;
-				// $location.path('/data');
+				vm.repositories = data;
 			});
-	};
-});
+
+		vm.submitRepoUrl = function() {
+			vm.processing = true;
+			
+			Repo.add(vm.repoUrl)
+				.success(function(data) {
+					vm.processing = false;
+
+					vm.repoUrl = null;
+					// $location.path('/data');
+				});
+		};
+	});
